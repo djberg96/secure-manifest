eb82d39c3613a47bdccb61808acc33fe282b74e4
Daniel Berger <dberger@redhat.com>
2019-04-09 09:52:06 -0400

    Added some comments.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,5 +1,3 @@
-require 'tmpdir'
-
 # Look for a manifest file and compare the gem's contents against
 # that file. If they do not match, or a manifest file isn't found,
 # then give the user the opportunity to bail out of installation.
@@ -15,6 +13,10 @@ Gem.pre_install do |installer|
 
   if manifest
     puts "Manifest file is: #{manifest}."
+    # TODO:
+    #   1 - Read file contents of manifest, ignore markup.
+    #   2 - Compare package contents against manifest.
+    #   3 - Give user option to bail out if contents and manifest do not match.
   else
     puts "Manifest file not found. Do you wish to continue?"
 
